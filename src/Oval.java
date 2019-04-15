c668da2900a7187cf8f138c64720ad915e4b3c59
Aaron Craig <aaron.t.craig-1@ou.edu>
2019-04-15 11:40:19 -0500

    created oval class and nearly finished it

--- src/Oval.java
+++ src/Oval.java
@@ -1,15 +1,16 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
 
 public class Oval extends Shape
 {
     private int diameter1;
     private int diameter2;
     
-    public Oval(Color color, boolean filled)
-    {
+    public Oval(Point pointUL, int diameter1, int diameter2, Color color, boolean filled)    {
         super(color, filled);
-        
+        this.diameter1 = diameter1;
+        this.diameter2 = diameter2;
     }
 
     @Override
@@ -17,6 +18,12 @@ public class Oval extends Shape
     {
         // TODO Auto-generated method stub
         
+    }
+    public int getDiameter1() {
+        return this.diameter1;
+    }
+    public int getDiameter2() {
+        return this.diameter2;
     }
 
 
