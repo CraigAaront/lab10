7c4af7c006fbd3b4a3f0d21270664b3f46ef5a88
Aaron Craig <aaron.t.craig-1@ou.edu>
2019-04-15 09:50:24 -0500

    initial commit

---
+++ src/Shape.java
@@ -0,0 +1,11 @@
+import java.awt.Color;
+import java.awt.Point;
+
+public abstract class Shape
+{
+    private Color color;
+    private Boolean filled;
+    protected Point[] location;
+    
+    
+}
